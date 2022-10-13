302b04fbafbc9f8f4c7b0d2685b548cd5711b04a
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-14 03:34:57 +0530

    Added all the event listeners and the end of tut 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 const penColor=document.querySelector('input[name="penColor"]');
 const penSize=document.querySelector('input[name="penSize"]');
-const saver=document.querySelector('#save');
+const save=document.querySelector('#save');
 
 context.strokeStyle='#000000';
 context.lineJoin='round';
@@ -12,4 +12,20 @@ let pen ={
     x:0,
     y:0,
     down:false
-}
+};
+save.addEventListener('click',()=>{
+    // console.log("Saved");
+});
+canvas.addEventListener('mousedown',()=>{
+    pen.down=false;
+    // console.log("MouseDown");
+});
+canvas.addEventListener('mousemove',()=>{
+    // console.log("MouseMove");
+});
+canvas.addEventListener('mouseup',()=>{
+    // console.log("MouseUp");
+});
+canvas.addEventListener('mouseout',()=>{
+    // console.log("MouseOut");
+});
